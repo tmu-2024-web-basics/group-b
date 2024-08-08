af719c0031511aac0d10d1b1e594858f50d3089c
rewaita <kyuri9ri9ri@gmail.com>
2024-08-08 19:36:19 +0900

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,351 @@
+//モーダルウィンドウ
+let btns = document.getElementsByClassName("nextModalBtn");
+let allModal = document.getElementsByClassName("modal");
+
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+
+        document.getElementById('startFC').style.display = 'block';
+        document.getElementById('topTitle').style.display = 'block';
+        document.getElementById('nori-1-icon').style.display = 'block';
+        document.getElementById('edamame-1-icon').style.display = 'block';
+        document.getElementById('goma-1-icon').style.display = 'block';
+        document.getElementById('nitamago-1-icon').style.display = 'block';
+        document.getElementById('shiso-1-icon').style.display = 'block';
+        document.getElementById('sake-1-icon').style.display = 'block';
+        document.getElementById('ume-1-icon').style.display = 'block';
+        document.getElementById('yaki-1-icon').style.display = 'block';
+
+    }
+}
+
+document.getElementById('finishBtn1').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result1').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+document.getElementById('finishBtn2').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result2').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+document.getElementById('finishBtn3').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result3').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+document.getElementById('finishBtn4').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result4').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+document.getElementById('finishBtn5').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result5').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+document.getElementById('finishBtn6').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result6').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+document.getElementById('finishBtn7').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result7').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+document.getElementById('finishBtn8').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result8').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+document.getElementById('finishBtn9').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result9').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+document.getElementById('finishBtn10').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result10').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+document.getElementById('finishBtn11').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result11').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+document.getElementById('finishBtn12').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result12').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+document.getElementById('finishBtn13').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result13').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+document.getElementById('finishBtn14').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result14').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+document.getElementById('finishBtn15').onclick = function(event){
+
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+    document.getElementById('result15').style.display = 'none';
+    
+    document.getElementById('startFC').style.display = 'block';
+    document.getElementById('topTitle').style.display = 'block';
+    document.getElementById('nori-1-icon').style.display = 'block';
+    document.getElementById('edamame-1-icon').style.display = 'block';
+    document.getElementById('goma-1-icon').style.display = 'block';
+    document.getElementById('nitamago-1-icon').style.display = 'block';
+    document.getElementById('shiso-1-icon').style.display = 'block';
+    document.getElementById('sake-1-icon').style.display = 'block';
+    document.getElementById('ume-1-icon').style.display = 'block';
+    document.getElementById('yaki-1-icon').style.display = 'block';
+}
+
+function closeAllModal() {
+    for (let i = 0; i < allModal.length; i++) {
+        allModal[i].style.display = "none";
+    }
+}
+
+function showLoading() {
+    document.getElementById('loading').style.display = 'block';
+}
+
+function hideLoading() {
+    document.getElementById('loading').style.display = 'none';
+}
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].onclick = function() {
+        closeAllModal();
+
+        let modalId = this.getAttribute("data-modal");
+        let modal = document.getElementById(modalId);
+
+        if(modalId=="q0"){
+
+            document.getElementById('startFC').style.display = 'none';
+            document.getElementById('topTitle').style.display = 'none';
+            document.getElementById('nori-1-icon').style.display = 'none';
+            document.getElementById('edamame-1-icon').style.display = 'none';
+            document.getElementById('goma-1-icon').style.display = 'none';
+            document.getElementById('nitamago-1-icon').style.display = 'none';
+            document.getElementById('shiso-1-icon').style.display = 'none';
+            document.getElementById('sake-1-icon').style.display = 'none';
+            document.getElementById('ume-1-icon').style.display = 'none';
+            document.getElementById('yaki-1-icon').style.display = 'none';
+            
+            
+            showLoading();
+            setTimeout(() => {
+                hideLoading();
+                modal.style.display = "block";
+                
+            }, 2000);
+        }else if(modalId=="result1" || modalId=="result2" || modalId=="result3" 
+        || modalId=="result4" || modalId=="result5" || modalId=="result6" || modalId=="result7" 
+        || modalId=="result8" || modalId=="result9" || modalId=="result10" || modalId=="result11" 
+        || modalId=="result12" || modalId=="result13" || modalId=="result14" || modalId=="result15"){
+            showLoading();
+            setTimeout(() => {
+                hideLoading();
+                modal.style.display = "block";
+                
+            }, 2000);
+            
+        }else{modal.style.display = "block";}
+    }
+}
+
+
+    
